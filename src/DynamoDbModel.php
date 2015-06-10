de977c3707ef0da3fd8b04148e9173e6c9aa02dd
Bao Pham <gbaopham@gmail.com>
2015-06-10 00:19:31 -0700

    Refactor and implement delete

--- src/DynamoDbModel.php
+++ src/DynamoDbModel.php
@@ -4,8 +4,8 @@ namespace BaoPham\DynamoDb;
 
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Log;
 
 /**
  * Class DynamoDbModel
@@ -62,10 +62,10 @@ abstract class DynamoDbModel extends Model
         $this->attributeFilter->filter($this->attributes);
 
         try {
-             $this->client->putItem([
-                 'TableName' => $this->getTable(),
-                 'Item' => $this->marshaler->marshalItem($this->attributes),
-             ]);
+            $this->client->putItem([
+                'TableName' => $this->getTable(),
+                'Item' => $this->marshaler->marshalItem($this->attributes),
+            ]);
 
             return true;
         } catch (Exception $e) {
@@ -90,25 +90,25 @@ abstract class DynamoDbModel extends Model
 
     public function delete()
     {
-        // TODO
+        $query = [
+            'TableName' => $this->getTable(),
+            'Key' => static::getDynamoDbKey($this, $this->getKey())
+        ];
+
+        $result = $this->client->deleteItem($query);
+        $status = array_get($result->toArray(), '@metadata.statusCode');
+
+        return $status == 200;
     }
 
     public static function find($id, array $columns = [])
     {
         $model = new static;
 
-        $keyName = $model->getKeyName();
-
-        $idKey = $model->marshaler->marshalItem([
-            $keyName => $id
-        ]);
-
         $query = [
             'ConsistentRead' => true,
             'TableName' => $model->getTable(),
-            'Key'       => [
-                $keyName => $idKey[$keyName]
-            ]
+            'Key' => static::getDynamoDbKey($model, $id)
         ];
 
         if (!empty($columns)) {
@@ -117,13 +117,19 @@ abstract class DynamoDbModel extends Model
 
         $item = $model->client->getItem($query);
 
-        $item = $item->toArray();
+        $item = array_get($item->toArray(), 'Item');
 
-        $item = array_get($item, 'Item');
+        if (empty($item)) {
+            return null;
+        }
 
         $item = $model->marshaler->unmarshalItem($item);
 
-        return (object) $item;
+        $model->fill($item);
+
+        $model->id = $id;
+
+        return $model;
     }
 
     public static function all($columns = [], $limit = -1)
@@ -149,13 +155,19 @@ abstract class DynamoDbModel extends Model
         $results = [];
 
         foreach ($items as $item) {
-            $results[] = (object) $model->marshaler->unmarshalItem($item);
+            $attributes = $model->marshaler->unmarshalItem($item);
+
+            $newModel = new static($attributes);
+
+            $newModel->id = $attributes[$model->primaryKey];
+
+            $results[] = $newModel;
         }
 
         return new Collection($results);
     }
 
-    public function first($columns = [])
+    public static function first($columns = [])
     {
         $item = static::all($columns, 1);
         return $item->first();
@@ -164,5 +176,20 @@ abstract class DynamoDbModel extends Model
     public static function where(array $attributes)
     {
         // TODO
+    }
+
+    protected static function getDynamoDbKey(DynamoDbModel $model, $id)
+    {
+        $keyName = $model->getKeyName();
+
+        $idKey = $model->marshaler->marshalItem([
+            $keyName => $id
+        ]);
+
+        $key = [
+            $keyName => $idKey[$keyName]
+        ];
+
+        return $key;
     }
 }
