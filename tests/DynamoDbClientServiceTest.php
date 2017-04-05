0b6fbefdb417139a81eba02acfe3e8f8dff45004
michaelshaffer37 <michael@hbrisk.com>
2017-04-05 10:39:25 -0700

    Adds some tests for the New static Setter/Getter for ClientService
    This commit adds some unit tests for the new method of configuring
    the DynamoDbClientService set on the DynamoDbModels.

---
+++ tests/DynamoDbClientServiceTest.php
@@ -0,0 +1,69 @@
+<?php
+
+/**
+ * Class DynamoDbClientServiceTest
+ */
+class DynamoDbClientServiceTest extends TestCase
+{
+    /**
+     * Test that the dynamoDbClientService is not initialized without the service provider being called.
+     *
+     * @test
+     */
+    public function testGetDynamoDbClientServiceUninitialized()
+    {
+        $dynamoDbClientService = \BaoPham\DynamoDb\DynamoDbModel::getDynamoDbClientService();
+
+        $this->assertNull(
+            $dynamoDbClientService,
+            'The default value of the DynamoDbClientService set on the DynamoDbModels should be null.'
+        );
+    }
+
+    /**
+     * Test that the getter and setter behave as expected for the DynamoDbClientService
+     *
+     * @test
+     */
+    public function testSetGetDynamoDbClientService()
+    {
+        $mockClientService = $this->getMockBuilder(\BaoPham\DynamoDb\DynamoDbClientInterface::class)->getMock();
+
+        \BaoPham\DynamoDb\DynamoDbModel::setDynamoDbClientService($mockClientService);
+
+        $dynamoDbClientService = \BaoPham\DynamoDb\DynamoDbModel::getDynamoDbClientService();
+
+        $this->assertInstanceOf(
+            get_class($mockClientService),
+            $dynamoDbClientService,
+            'The returned DynamoDbClientService should be the same as the value provided to the setter.'
+        );
+    }
+
+    /**
+     * The unset method for the DynamoDbClientService should cause the internal DynamoDbClientService to be set to null
+     *
+     * @test
+     */
+    public function testUnsetDynamoDbClientService()
+    {
+        \BaoPham\DynamoDb\DynamoDbModel::unsetDynamoDbClientService();
+
+        $dynamoDbClientService = \BaoPham\DynamoDb\DynamoDbModel::getDynamoDbClientService();
+
+        $this->assertNull(
+            $dynamoDbClientService,
+            'After un-setting the DynamoDbClientService on the DynamoDbModel the getter should return null.'
+        );
+    }
+
+    /**
+     * Make sure we are not leaving any values set on the DynamoDbModel
+     */
+    public function tearDown()
+    {
+        parent::tearDown();
+
+        \BaoPham\DynamoDb\DynamoDbModel::unsetDynamoDbClientService();
+    }
+}
