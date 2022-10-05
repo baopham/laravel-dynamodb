19d615655265ead109e2f76f043f1e03e0dc8d80
zgrguric <zgrguric@xrplwin.com>
2022-10-05 14:04:25 +0200

    Add pagedCount method to query builder
    This method returns detailed counts and evaluated object which can be used for paginated count.

--- src/DynamoDbQueryBuilder.php
+++ src/DynamoDbQueryBuilder.php
@@ -676,6 +676,22 @@ class DynamoDbQueryBuilder
 
         return $res['Count'];
     }
+    
+    public function pagedCount()
+    {
+        $limit = isset($this->limit) ? $this->limit : static::MAX_LIMIT;
+
+        $raw = $this->toDynamoDbQuery(['count(*)'], $limit);
+    
+        if ($raw->op === 'Scan') {
+            $res = $this->client->scan($raw->query);
+        } else {
+            $res = $this->client->query($raw->query);
+        }
+        $this->lastEvaluatedKey = Arr::get($res, 'LastEvaluatedKey');
+        
+        return (object)['count' => $res['Count'], 'scanned_count' => $res['ScannedCount'], 'lastKey' => $this->lastEvaluatedKey];
+    }
 
     public function decorate(Closure $closure)
     {
