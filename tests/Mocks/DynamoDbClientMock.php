4bb82704df42ac0328efb98d2e1d86584ab74984
Claudio La Barbera <claudio.labarbera@everli.com>
2024-04-02 17:46:39 +0200

    fix(style): opening brace should be on a new line

--- tests/Mocks/DynamoDbClientMock.php
+++ tests/Mocks/DynamoDbClientMock.php
@@ -7,27 +7,33 @@ use Aws\Result;
 
 class DynamoDbClientMock extends DynamoDbClient
 {
-    public function putItem(array $args = []): Result {
+    public function putItem(array $args = []): Result
+    {
         return parent::putItem($args);
     }
 
-    public function updateItem(array $args = []): Result {
+    public function updateItem(array $args = []): Result
+    {
         return parent::updateItem($args);
     }
 
-    public function deleteItem(array $args = []): Result {
+    public function deleteItem(array $args = []): Result
+    {
         return parent::deleteItem($args);
     }
 
-    public function scan(array $args = []): Result {
+    public function scan(array $args = []): Result
+    {
         return parent::scan($args);
     }
 
-    public function query(array $args = []): Result {
+    public function query(array $args = []): Result
+    {
         return parent::query($args);
     }
 
-    public function batchWriteItem(array $args = []): Result {
+    public function batchWriteItem(array $args = []): Result
+    {
         return parent::batchWriteItem($args);
     }
 }
